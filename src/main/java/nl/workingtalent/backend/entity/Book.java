b05a4d3d87ca40bee5ba18329bc9c863f27b4668
Martijn Hiemstra <info@greenhighway.nl>
2023-08-31 11:21:02 +0200

    Added book entity

---
+++ src/main/java/nl/workingtalent/backend/entity/Book.java
@@ -0,0 +1,37 @@
+package nl.workingtalent.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(length = 100, nullable = false)
+	private String title;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	
+}
