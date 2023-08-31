a633906f4c88de4f11268bc2088ed8d87fa6f7ec
mcbiggah99 <mahircan@me.com>
2023-08-31 12:34:49 +0200

    Added getters and setters for book

--- src/main/java/nl/workingtalent/backend/entity/Book.java
+++ src/main/java/nl/workingtalent/backend/entity/Book.java
@@ -50,6 +50,54 @@ public class Book {
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
+	public int getEdition() {
+		return edition;
+	}
+
+	public void setEdition(int edition) {
+		this.edition = edition;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCollation() {
+		return collation;
+	}
+
+	public void setCollation(String collation) {
+		this.collation = collation;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
 	
 	
 	
