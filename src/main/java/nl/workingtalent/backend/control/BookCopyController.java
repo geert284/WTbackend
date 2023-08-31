cd5b71e0b644de04d75b9296bc4a14a95a20734d
judyhulscher <judy.hulscher@live.nl>
2023-08-31 15:00:15 +0200

    Set-up repo, service and controller files

--- src/main/java/nl/workingtalent/backend/control/BookCopyController.java
+++ src/main/java/nl/workingtalent/backend/control/BookCopyController.java
@@ -1,5 +1,19 @@
 package nl.workingtalent.backend.control;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import nl.workingtalent.backend.entity.BookCopy;
+import nl.workingtalent.backend.service.BookCopyService;
+
+@CrossOrigin
+@RestController
 public class BookCopyController {
+	
+	@Autowired
+	private BookCopyService service;
 
 }
