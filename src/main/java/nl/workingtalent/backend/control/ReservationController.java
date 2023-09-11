9fbf0854b9f7f1e83bf5e45b249f16ddcd2a2b81
Geert Jansen <geert.jansen95@hotmail.com>
2023-09-11 10:54:33 +0200

    added the missing 4 dtos to the controllers

--- src/main/java/nl/workingtalent/backend/control/ReservationController.java
+++ src/main/java/nl/workingtalent/backend/control/ReservationController.java
@@ -1,5 +1,6 @@
 package nl.workingtalent.backend.control;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.workingtalent.backend.dto.ReservationDto;
 import nl.workingtalent.backend.entity.Reservation;
 import nl.workingtalent.backend.service.ReservationService;
 
@@ -18,7 +20,22 @@ public class ReservationController {
 	private ReservationService service;
 	
 	@RequestMapping("reservation/all")
-	public List<Reservation> getReservations() {
-		return service.findAllReservations();
+	public List<ReservationDto> getReservations() {
+		List<Reservation> reservations = service.findAllReservations();
+		
+		List<ReservationDto> dtos = new ArrayList<>();
+		
+		reservations.forEach(reservation ->{
+			ReservationDto dto = new ReservationDto();
+			dto.setId(reservation.getId());
+			dto.setProcessed(reservation.isProcessed());
+			dto.setReservationDate(reservation.getReservationDate());
+			dto.setAccount_id(reservation.getAccount().getId());
+			dto.setBookCopy_id(reservation.getBookCopy().getId());
+			
+			dtos.add(dto);
+		});
+		
+		return dtos;
 	}
 }
